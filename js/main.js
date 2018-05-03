aa3f0efd32817483f311eb58b6d4320f90a6f657
nd4pa <antoine.blondeau@isen-bretagne.fr>
2018-05-03 20:42:33 +0200

    Remove out of date stuff add RSS reader for medium

---
+++ js/main.js
@@ -0,0 +1,6 @@
+jQuery(function($) {
+      $("#rss-feeds").rss("https://medium.com/feed/@ant.blondeauw",
+      {
+        entryTemplate:'<article><a class="image">{teaserImage}</a><div class="inner"><h4><a href="{url}">{title}</a></h4><p>{shortBodyPlain}</p><p>{author}, {date}</p></div></article>'
+      })
+})
